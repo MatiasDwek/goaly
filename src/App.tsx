1e2c192dcbede580b190f2f9716fca229aef228c
Matias Dwek <matiasdwek@gmail.com>
2022-07-17 19:56:54 +0200

    Extract calendar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import { useState, useEffect } from 'react';
-import { initializeCalendar } from './reducers/calendarReducer';
-import { useAppDispatch, useAppSelector } from './hooks';
+import Calendar from './components/calendar';
 
 function App() {
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(initializeCalendar()) 
-  },[dispatch]);
-
-  const calendar = useAppSelector((state) => state.calendar);
   return (
-    <div>
-      <h1>Task calendar</h1>
-      <ul>
-        {calendar.map(day =>
-          <li key={day.date}>
-            Day: {epochToDay(day.date)} Day score: {day.points}
-          </li>)}
-      </ul>
-    </div>
+      <Calendar />
   );
-}
-
-function epochToDay(epochDate: number): string {
-  const date = new Date(epochDate);
-  const stringDate = date.toLocaleString();
-  return stringDate.substring(stringDate.indexOf(","),length)
 }
 
 export default App;
