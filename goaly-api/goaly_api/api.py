7a48219b513b1a333ae6a5c3d9786da6ef8c14fe
Martina Dwek <martinadwek@gmail.com>
2022-07-24 15:23:54 -0700

    Added post in api

--- goaly-api/goaly_api/api.py
+++ goaly-api/goaly_api/api.py
@@ -1,54 +1,73 @@
 import time
 from flask import Flask
 from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
-
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
 
 @app.route('/api/completed-tasks')
 def get_completed_tasks():
     return jsonify([
     {
-      "id": 1,
-      "date": 1658076446963,
-      "points": 1
+      "date": "2022/6/24",
+      "taskId": 1,
+      "id": 1
     },
     {
-      "id": 2,
-      "date": 1649004471000,
-      "points": 5
+      "date": "2022/6/24",
+      "taskId": 2,
+      "id": 2
     },
     {
-      "id": 3,
-      "date": 1642697271000,
-      "points": 1
+      "date": "2022/6/24",
+      "taskId": 3,
+      "id": 3
     },
     {
-      "date": 1657843200000,
-      "points": 1,
+      "date": "2022/6/24",
+      "taskId": 4,
       "id": 4
     },
     {
-      "date": 1657929600000,
-      "points": 1,
+      "date": "2022/6/23",
+      "taskId": 2,
       "id": 5
-    },
-    {
-      "date": 1657238400000,
-      "points": 1,
-      "id": 6
-    },
-    {
-      "date": 1657756800000,
-      "points": 1,
-      "id": 7
-    },
-    {
-      "date": 1641340800000,
-      "points": 1,
-      "id": 8
     }
-    ])
+  ])
+
+@app.route('/api/tasks')
+def get_tasks():
+    return jsonify([
+    {
+      "title": "Go to the gym",
+      "points": 3,
+      "creationDate": "2022-07-23T16:38:42.848Z",
+      "id": 1
+    },
+    {
+      "title": "Read a book",
+      "points": 2,
+      "creationDate": "2022-07-23T16:47:11.578Z",
+      "id": 2
+    },
+    {
+      "title": "Hacer API",
+      "points": 3,
+      "creationDate": "2022-07-24T19:40:31.172Z",
+      "id": 3
+    },
+    {
+      "title": "hola",
+      "points": 1,
+      "creationDate": "2022-07-24T19:40:43.921Z",
+      "id": 4
+    }
+  ])
+
+#POST
+
+@app.route('/api/tasks', methods=['POST'])
+def create_tasks():
+    input_json = request.get_json(force=True) 
+    print(input_json)
+    return "a"
