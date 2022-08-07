dfe88d154867c971f6cd3db50f45c19cea6e3cc4
Martina Dwek <martinadwek@gmail.com>
2022-08-06 23:30:31 -0700

    Updated api complete-task

--- goaly-api/goaly_api/api.py
+++ goaly-api/goaly_api/api.py
@@ -7,6 +7,9 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+db_tasks = []
+db_completed_tasks = []
+
 @app.get('/api/completed-tasks')
 def get_completed_tasks():
     return jsonify(db_completed_tasks)
@@ -15,16 +18,23 @@ def get_completed_tasks():
 def get_tasks():
     return jsonify(db_tasks)
 
-#POST
-
-db_tasks = []
-db_completed_tasks = []
-
 @app.post('/api/completed-tasks')
 def complete_tasks():
-    input_json = request.get_json() 
-    db_completed_tasks.append(input_json)
-    return input_json
+    input_json = request.get_json()
+    new_completed_task = {}
+    new_completed_task["points"] = input_json.get("points")
+    if new_completed_task["points"] is None:
+      return "Points not specified", 400 
+    if new_completed_task["points"] > 2:
+      return "Points larger than maximum", 400
+    new_completed_task["id"] = input_json["id"]
+    if new_completed_task["id"] is None:
+      return "id not specified", 400
+    new_completed_task["time"] = time.time()
+    #corregir el formato del time
+    #solo agregué los que son iguales a create_task. Hay que revisar si no falta nada más.
+    db_completed_tasks.append(new_completed_task)
+    return new_completed_task
 
 @app.post('/api/tasks')
 def create_task():
@@ -37,7 +47,8 @@ def create_task():
       return "Points larger than maximum", 400
     new_task["title"] = input_json["title"]
     new_task["id"] = int(random.random()*100000)
-    new_task["date"] = 1
+    new_task["time"] = time.time()
+    #corregir el formato del time
     db_tasks.append(new_task)
     return new_task
     #recuperar la fecha
