d2dfd55ed5eee6004ffc0b7f0e94cf202582aa0e
Martina Dwek <72419696+martinadwek@users.noreply.github.com>
2022-10-29 13:52:41 -0700

    Add basic BE api implementation
    * wip
    
    * Update endpoint URL
    
    * Fix json-server routes
    
    * Add .eslintrc config
    
    * Add tasks creation handling
    
    * Change completed tasks date format
    
    * Add task list component
    
    * Rename components
    
    * Make it possible to complete a task
    
    * Tidy up dev data
    
    * Delete CompleteTaskForm
    
    * Add checkbox when completing task
    
    * Make day check in dynamic
    
    * Added post in api
    
    * Update task to use python list
    
    * wip
    
    * Fix error
    
    * Return proper payload in create_tasks
    
    * api update of post task
    
    * Updated api complete-task
    
    * Fix endpoints
    
    Co-authored-by: Matias Dwek <matiasdwek@gmail.com>

--- goaly-api/goaly_api/api.py
+++ goaly-api/goaly_api/api.py
@@ -1,8 +1,73 @@
+import logging
 import time
+
 from flask import Flask
+from flask import jsonify
+from flask import request
+
+from .api_helpers import generate_id, validate_date
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+tasks_db = {}
+completed_tasks_db = {}
+
+
+@app.get("/api/completed-tasks")
+def get_completed_tasks():
+    return jsonify(list(completed_tasks_db.values()))
+
+
+@app.get("/api/tasks")
+def get_tasks():
+    return jsonify(list(tasks_db.values()))
+
+
+@app.post("/api/tasks")
+def create_task():
+    input_task = request.get_json()
+    title = input_task.get("title")
+    if not title:
+        return "Task title cannot be empty", 400
+    if len(title) > 200:
+        return "Task title is too long", 400
+
+    task_id = generate_id()
+    if task_id in tasks_db:
+        logging.error(f"Error creating task {task_id}, duplicated id")
+        return "Error creating task, try again", 500
+
+    new_task = {"id": task_id, "title": title, "creationDate": time.time()}
+    tasks_db[task_id] = new_task
+    return new_task
+
+
+@app.post("/api/completed-tasks")
+def complete_task():
+    input_completed_task = request.get_json()
+    task_id = input_completed_task.get("taskId")
+    if task_id not in tasks_db:
+        return "Task id not found", 404
+
+    date = input_completed_task.get("date")
+    if not date and not validate_date(date):
+        return "Missing completion date or wrong date format", 400
+
+    completed_task_id = generate_id()
+    if completed_task_id in tasks_db:
+        logging.error(
+            f"Error completing task {task_id}, duplicated id {completed_task_id}"
+        )
+        return "Error completing task, try again", 500
+
+    new_completed_task = {
+        "id": completed_task_id,
+        "taskId": task_id,
+        "date": date,
+    }
+    completed_tasks_db[completed_task_id] = new_completed_task
+    return new_completed_task
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ goaly-api/goaly_api/api_helpers.py
@@ -0,0 +1,20 @@
+import datetime
+import random
+import string
+
+
+def generate_id() -> str:
+    """
+    Generates a random id in [a-z0-9]{3}-[a-z0-9]{3}-[a-z0-9]{3} format
+    """
+    id_parts = ["".join(random.choices(string.ascii_lowercase + string.digits, k=3)) for _ in range(3)]
+    return "-".join(id_parts)
+
+
+def validate_date(date: str) -> bool:
+    try:
+        year, month, day = date.split("/")
+        datetime.datetime(year=year, month=month, day=day)
+    except:
+        return False
+    return True
