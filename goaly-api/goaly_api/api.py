01ee46859ac47d451fbdd45647330c59645a7fa6
Matias Dwek <matiasdwek@gmail.com>
2022-10-29 22:51:25 +0200

    Fix endpoints

--- goaly-api/goaly_api/api.py
+++ goaly-api/goaly_api/api.py
@@ -1,54 +1,73 @@
+import logging
 import time
+
 from flask import Flask
 from flask import jsonify
 from flask import request
-import random
-from datetime import datetime
+
+from .api_helpers import generate_id, validate_date
 
 app = Flask(__name__)
 
-db_tasks = []
-db_completed_tasks = []
+tasks_db = {}
+completed_tasks_db = {}
 
-@app.get('/api/completed-tasks')
+
+@app.get("/api/completed-tasks")
 def get_completed_tasks():
-    return jsonify(db_completed_tasks)
+    return jsonify(list(completed_tasks_db.values()))
 
-@app.get('/api/tasks')
+
+@app.get("/api/tasks")
 def get_tasks():
-    return jsonify(db_tasks)
+    return jsonify(list(tasks_db.values()))
 
-@app.post('/api/completed-tasks')
-def complete_tasks():
-    input_json = request.get_json()
-    new_completed_task = {}
-    new_completed_task["points"] = input_json.get("points")
-    if new_completed_task["points"] is None:
-      return "Points not specified", 400 
-    if new_completed_task["points"] > 2:
-      return "Points larger than maximum", 400
-    new_completed_task["id"] = input_json["id"]
-    if new_completed_task["id"] is None:
-      return "id not specified", 400
-    new_completed_task["time"] = time.time()
-    #corregir el formato del time
-    #solo agregué los que son iguales a create_task. Hay que revisar si no falta nada más.
-    db_completed_tasks.append(new_completed_task)
+
+@app.post("/api/tasks")
+def create_task():
+    input_task = request.get_json()
+    title = input_task.get("title")
+    if not title:
+        return "Task title cannot be empty", 400
+    if len(title) > 200:
+        return "Task title is too long", 400
+
+    task_id = generate_id()
+    if task_id in tasks_db:
+        logging.error(f"Error creating task {task_id}, duplicated id")
+        return "Error creating task, try again", 500
+
+    new_task = {"id": task_id, "title": title, "creationDate": time.time()}
+    tasks_db[task_id] = new_task
+    return new_task
+
+
+@app.post("/api/completed-tasks")
+def complete_task():
+    input_completed_task = request.get_json()
+    task_id = input_completed_task.get("taskId")
+    if task_id not in tasks_db:
+        return "Task id not found", 404
+
+    date = input_completed_task.get("date")
+    if not date and not validate_date(date):
+        return "Missing completion date or wrong date format", 400
+
+    completed_task_id = generate_id()
+    if completed_task_id in tasks_db:
+        logging.error(
+            f"Error completing task {task_id}, duplicated id {completed_task_id}"
+        )
+        return "Error completing task, try again", 500
+
+    new_completed_task = {
+        "id": completed_task_id,
+        "taskId": task_id,
+        "date": date,
+    }
+    completed_tasks_db[completed_task_id] = new_completed_task
     return new_completed_task
 
-@app.post('/api/tasks')
-def create_task():
-    input_json = request.get_json()
-    new_task = {}
-    new_task["points"] = input_json.get("points")
-    if new_task["points"] is None:
-      return "Points not specified", 400
-    if new_task["points"] > 2:
-      return "Points larger than maximum", 400
-    new_task["title"] = input_json["title"]
-    new_task["id"] = int(random.random()*100000)
-    new_task["time"] = time.time()
-    #corregir el formato del time
-    db_tasks.append(new_task)
-    return new_task
-    #recuperar la fecha
+
+if __name__ == "__main__":
+    app.run()
