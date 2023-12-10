c95172959622195b815477a5207011081e292839
Matias Dwek <matiasdwek@gmail.com>
2023-12-10 21:14:55 +0100

    Use a connection pool with psycopg

--- goaly-api/api/db/schema.py
+++ goaly-api/api/db/schema.py
@@ -1,66 +1,71 @@
 import logging
 
-import psycopg
+from .db import pool
 
 
 # Create the database tables
-def create_tables(db_url: str):
+def create_tables():
     logging.info("Creating database tables...")
 
-    # Connect to an existing database
-    with psycopg.connect(db_url) as conn:
-        # Open a cursor to perform database operations
-        with conn.cursor() as cur:
-            # Create the database
-            cur.execute("CREATE DATABASE IF NOT EXISTS tasks;")
+    with pool.connection() as conn:
+        cur = conn.cursor()
+        # Create the database
+        cur.execute("CREATE DATABASE IF NOT EXISTS tasks;")
 
-            # Create the `users` table
-            # -------------------------------------------------
-            # | user_id            | username | email             | ... |
-            # -------------------------------------------------
-            # | abc-def-ghi       | user1    | user1@example.com | ... |
-            users_table = """
-                CREATE TABLE IF NOT EXISTS users (
-                  user_id VARCHAR(11) NOT NULL PRIMARY KEY,
-                  username VARCHAR(255) NOT NULL UNIQUE,
-                  email VARCHAR(255) NOT NULL UNIQUE,
-                  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                );
+        # Create the `users` table
+        # -------------------------------------------------
+        # | user_id            | username | email             | ... |
+        # -------------------------------------------------
+        # | abc-def-ghi       | user1    | user1@example.com | ... |
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+              user_id VARCHAR(11) NOT NULL PRIMARY KEY,
+              username VARCHAR(255) NOT NULL UNIQUE,
+              email VARCHAR(255) NOT NULL UNIQUE,
+              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
             """
-            cur.execute(users_table)
+                    )
 
-            # Create the `tasks` table
-            # --------------------------------------
-            # | task_id            | task_name | creation_date |
-            # --------------------------------------
-            # | abc-def-ghi       | Task 1    | 2023-04-23    |
-            cur.execute(
-                """
-                CREATE TABLE IF NOT EXISTS tasks (
-                  task_id VARCHAR(11) NOT NULL PRIMARY KEY,
-                  user_id VARCHAR(11) NOT NULL,
-                  name VARCHAR(255) NOT NULL,
-                  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                  FOREIGN KEY (user_id) REFERENCES users(user_id)
-                );
+        # Create the `tasks` table
+        # --------------------------------------
+        # | task_id            | task_name | creation_date |
+        # --------------------------------------
+        # | abc-def-ghi       | Task 1    | 2023-04-23    |
+        cur.execute(
             """
-            )
-
-            # Create the `completed_tasks` table
-            # ------------------------------------------------------------
-            # | completed_task_id | user_id           | task_id         | completion_date |
-            # ------------------------------------------------------------
-            # | abc-def-ghi      | abc-def-ghi       | abc-def-ghi     | 2023-04-23      |
-            cur.execute(
-                """
-                CREATE TABLE IF NOT EXISTS completed_tasks (
-                  task_id VARCHAR(11) NOT NULL,
-                  completed_at DATE NOT NULL,
-                  PRIMARY KEY (task_id, completed_at),
-                  FOREIGN KEY (task_id) REFERENCES tasks(task_id)
-                );
+            CREATE TABLE IF NOT EXISTS tasks (
+              task_id VARCHAR(11) NOT NULL PRIMARY KEY,
+              user_id VARCHAR(11) NOT NULL,
+              name VARCHAR(255) NOT NULL,
+              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+              FOREIGN KEY (user_id) REFERENCES users(user_id)
+            );
             """
-            )
+        )
 
-            # Make the changes to the database persistent
-            conn.commit()
+        # Create the `completed_tasks` table
+        # ------------------------------------------------------------
+        # | completed_task_id | user_id           | task_id         | completion_date |
+        # ------------------------------------------------------------
+        # | abc-def-ghi      | abc-def-ghi       | abc-def-ghi     | 2023-04-23      |
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS completed_tasks (
+              task_id VARCHAR(11) NOT NULL,
+              completed_at DATE NOT NULL,
+              PRIMARY KEY (task_id, completed_at),
+              FOREIGN KEY (task_id) REFERENCES tasks(task_id)
+            );
+            """
+        )
+
+        # Create root user for testing and development
+        cur.execute(
+            """
+            INSERT INTO users (user_id, username, email)
+            VALUES ('aaa-aaa-aaa', 'root-user', 'root-user@goaly.com')
+            ON CONFLICT (user_id)
+            DO NOTHING;
+            """
+        )
--- goaly-api/api/wsgi.py
+++ goaly-api/api/wsgi.py
@@ -6,15 +6,11 @@ from pythonjsonlogger import jsonlogger
 from .controllers import app
 from .db.schema import create_tables
 
-DEV_DB_URL = "postgresql://root:@cockroachlabs:26257/tasks"
-
 app_env = os.environ.get("APP_ENV", "dev")
 
 if app_env == "dev":
     logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.INFO)
     logger = logging.getLogger()
-
-    db_url = DEV_DB_URL
 else:
     logger = logging.getLogger()
     logHandler = logging.StreamHandler()
@@ -25,13 +21,12 @@ else:
     logger.setLevel(logging.INFO)
     logger.addHandler(logHandler)
 
-    db_url = os.environ["DATABASE_URL"]
-
 # Create the database tables
 try:
-    create_tables(db_url)
+    create_tables()
 except Exception as e:
-    logger.error(e)
+    logger.error(f"Error initing DB, exiting... {str(e)}", exc_info=True)
+    raise e
 
 if __name__ == "__main__":
     app.run()
